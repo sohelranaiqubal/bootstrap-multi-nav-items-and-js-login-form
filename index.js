da878f13f7d1b519c786e5b6330626cd3a484879
sohelranaiqubal <shoeliqubal7890@gmail.com>
2022-08-22 02:38:58 +0300

    index.html

---
+++ index.js
@@ -0,0 +1,28 @@
+var nav = document.querySelector('nav');
+ 
+ window.addEventListener('scroll',function(){ 
+  if(window.pageYOffset > 100){ 
+    nav.classList.add('bg-dark', 'shadow','text-white');
+  }else{ 
+    nav.classList.remove('bg-dark', 'shadow','text-white' );
+  }
+ })
+
+
+
+ const balcal = document.querySelector('#close');
+ const cancel = document.querySelector('.cancelbtn');
+ const loginBtn = document.querySelector('#loginBtn');
+
+ loginBtn.addEventListener('click', () => { 
+        loginform.style.display='block';
+      
+ });
+
+ balcal.addEventListener('click', () => { 
+      loginform.style.display='none';
+ });
+
+ cancel.addEventListener('click', () => { 
+  loginform.style.display='none';
+});
